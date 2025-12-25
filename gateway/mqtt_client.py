4893ac290b9725bddd8adc3f565bcf2ac18fe51e
saadiqbal09 <saadchavhan2@gmail.com>
2025-12-25 17:25:05 +0530

    Add MQTT publish test for PSLab gateway
    Added a publish_test function to publish messages to an MQTT topic.

--- gateway/mqtt_client.py
+++ gateway/mqtt_client.py
@@ -1,6 +1,20 @@
 import paho.mqtt.client as mqtt
+import time
+from paho.mqtt.enums import CallbackAPIVersion
 
-def connect_mqtt(broker="test.mosquitto.org", port=1883):
-    client = mqtt.Client()
-    client.connect(broker, port)
-    return client
+
+def publish_test():
+    client = mqtt.Client(CallbackAPIVersion.VERSION2)
+    client.connect("test.mosquitto.org", 1883, 60)
+
+    for i in range(5):
+        msg = f"test_value_{i}"
+        client.publish("pslab/test", msg)
+        print("Published:", msg)
+        time.sleep(1)
+
+    client.disconnect()
+
+if __name__ == "__main__":
+    publish_test()
+
